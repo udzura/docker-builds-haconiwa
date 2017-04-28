263088ee0ebb287218a8aa1e9fc3740370efcc99
Uchio KONDO <udzura@pepabo.com>
2017-04-28 16:17:26 +0900

    How to override version no

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # docker-builds-haconiwa
+
 The Docker base image that builds Haconiwa
+
+## Build deb
+
+```bash
+$ docker build -t udzura/builds-haconiwa-deb:0.0.1 deb/
+$ docker run \
+    -v`pwd`/pkg:/out/pkg \
+    -v/path/to/local/haconiwa:/build/haconiwa \ # If you want to specify source
+    -v`pwd`/example_build_config.rb:/build/build_config.rb \ # If want to customize build_config
+    --rm -t udzura/builds-haconiwa-deb:0.0.1
+## And then if you specify/override version, instead run:
+## This requires `/build/haconiwa' mounted
+#   --rm -t udzura/builds-haconiwa-deb:0.0.1 0.8.5test1
+```
--- deb/builddeb.sh
+++ deb/builddeb.sh
@@ -2,9 +2,11 @@
 set -xe
 
 export DEBIAN_FRONTEND=noninteractive
+export LANG=C
 
 VERSION=${1:-0.8.5}
 MAINTAINER=${2:-'udzura@udzura.jp'}
+MAINTAINER_NAME=${3:-'Uchio Kondo'}
 
 if test -d /build/haconiwa
 then
@@ -26,10 +28,24 @@ then
 fi
 
 cd /build/haconiwa-$VERSION
+    if ! grep -q 'VERSION = "'$VERSION'"' mrblib/haconiwa/version.rb; then
+        sed -i.old 's/.*VERSION.*/VERSION = "'$VERSION'"/' mrblib/haconiwa/version.rb
+        cp packages/deb/debian/changelog /tmp/.changelog
+        cat <<EOLOG > packages/deb/debian/changelog
+haconiwa ($VERSION-1) unstable; urgency=medium
+
+  * Customized deb build
+
+ -- $MAINTAINER_NAME <$MAINTAINER>  $(date "+%a, %e %b %Y %H:%M:%S %z")
+
+EOLOG
+        cat /tmp/.changelog >> packages/deb/debian/changelog
+    fi
+
     rake mruby
-    dh_make -s -e $MAINTAINER --createorig -y
+    dh_make -y -s -e $MAINTAINER --createorig -y
     cp -v packages/deb/debian/* debian/
     rm -rf debian/*.ex debian/*.EX
-    debuild -uc -us
+    debuild --no-tgz-check -uc -us
 cd ../
 cp -v *.deb /out/pkg
